eb53ed556746861f9c4773efc28f24535a5efb73
Hilton <18002446@vcconnect.co.za>
2020-09-19 12:02:47 +0200

    Added Cell Class

---
+++ app/src/main/java/com/higher/login_register_home/Cell.java
@@ -0,0 +1,28 @@
+package com.higher.login_register_home;
+
+//_____Gallery Code
+// Coded By: Hilton
+// Edited by:
+//Tested by:
+
+// this cell object is used for each picture/grid in the grid view
+public class Cell {
+
+    private String title, path;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
